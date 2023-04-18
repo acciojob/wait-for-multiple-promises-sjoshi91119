8f0214986af56bacb416a6653344843d68b9eaa7
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-18 12:44:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,3 +26,21 @@
 //       totalRow.insertCell(0).textContent = "Total";
 //       totalRow.insertCell(1).textContent = totalDuration;
 //     });
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
+});
+//your code here
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
+});
+module.exports = app;
